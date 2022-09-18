615699a1fde4cd51ed1eac790236934765231ca5
ggiifftt <110756025+ggiifftt@users.noreply.github.com>
2022-09-18 13:48:44 +0900

    Add files via upload

---
+++ 0918.py
@@ -0,0 +1,37 @@
+a = {1, 2, 3, 4, 5}
+b = {4, 5, 6, 7, 8}
+
+hap = a | b
+print(hap)
+
+cha = a - b
+print(cha)
+
+gyo = a & b
+print(gyo)
+
+print(f"합집합 = {hap}\n차집합 = {cha}\n교집합 = {gyo}")
+
+A = 7
+B = 3
+a, b = input().split()
+a = int(a)
+b = int(b)
+print('%d + %d = %d' %(a, b, a+b))
+print('{} - {} = {}'.format(a, b, a-b))
+print(f'{a} * {b} = {a * b}')
+print('%d / %d = %f' %(a, b, a/b))
+print('%d // %d = %d' %(a, b, a//b))
+print('%d %% %d = %d' %(a, b, a%b))
+
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \. \". L_r'")
+print("   `~\/")
+print("      |")
+print("      |")
+
+s = input()
+print(s + '??!')
+
